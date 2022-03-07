0556070980f14184c85ce37ccfbc257e493f5a3d
Yuji Takasaki <yuji08752@gmail.com>
2022-03-07 15:35:20 +0900

    router を追加

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="nav">
+  <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  </nav>
+  <router-view/>
 </template>
 
 <style>
@@ -15,16 +15,16 @@
   color: #2c3e50;
 }
 
-#nav {
+nav {
   padding: 30px;
 }
 
-#nav a {
+nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
+nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
